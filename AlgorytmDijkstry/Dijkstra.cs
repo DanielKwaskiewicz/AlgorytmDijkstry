b505f87eaf37f03908cb1fbbd1bc56dacaaff542
DanielKwaskiewicz <danielkwaskiewicz@gmail.com>
2019-07-09 19:48:01 +0200

    Initial Commit

---
+++ AlgorytmDijkstry/Dijkstra.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmDijkstry
+{
+    class Dijkstra
+    {
+        private int[,] adjacencyMatrix;
+        private double[] distance;
+        public Dijkstra(int nodeCount)
+        {
+            this.adjacencyMatrix = new int[nodeCount, nodeCount];
+            this.distance = new double[nodeCount];
+        }
+        public void SetAdjacencyMatrix(int m, int n, int value)
+        {
+            if (m == n)
+                adjacencyMatrix[m, n] = 0;  //odległość z miasta m do miasta m musi być zerowa
+            else
+            {
+                adjacencyMatrix[m, n] = value;
+                adjacencyMatrix[n, m] = value;  //graf nieskierowany - macierz jest symetryczna
+            }
+        }
+        public int[,] GetAdjacencyMatrix()
+        {
+            return adjacencyMatrix;
+        }
+        public void SetDistance(int cityNum, double value)
+        {
+            distance[cityNum] = value;
+        }
+        public double[] GetDistance()
+        {
+            return distance;
+        }
+        public void DisplayAdjacencyMatrix()
+        {
+            for (int m = 0; m < GetAdjacencyMatrix().GetLength(0); m++)
+            {
+                for (int n = 0; n < GetAdjacencyMatrix().GetLength(1); n++)
+                    Console.Write(GetAdjacencyMatrix()[m, n] + "\t");
+                Console.WriteLine();
+            }
+        }
+        public void NewEdge(int m, int n, int weight)
+        {
+            if (weight > 0)
+                SetAdjacencyMatrix(m, n, weight);
+        }
+        public void CreateEdges()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)    //połączenie dla każdego miasta
+            {
+                int r;
+                do
+                {
+                    r = rnd.Next(10);
+                } while (r == i);   //krawędź musi znajdować się między dwoma różnymi węzłami
+                SetAdjacencyMatrix(i, r, rnd.Next(1, 20));
+            }
+            for (int j = 0; j < 8; j++) //dodatkowe losowe połączenia
+            {
+                SetAdjacencyMatrix(rnd.Next(10), rnd.Next(10), rnd.Next(1, 20));
+            }
+        }
+        public List<int>[] CalculateDistance(int cityNum)
+        {
+            List<int>[] waypoints = new List<int>[GetAdjacencyMatrix().GetLength(0)];
+
+            for (int b = 0; b < GetAdjacencyMatrix().GetLength(0); b++)
+            {
+                SetDistance(b, double.PositiveInfinity);
+                waypoints[b] = new List<int>();
+            }
+            bool[] visited = new bool[GetAdjacencyMatrix().GetLength(0)];
+            SetDistance(cityNum, 0);
+
+            Queue<int> nodeQueue = new Queue<int>();
+            nodeQueue.Enqueue(cityNum);
+
+            while (nodeQueue.Count > 0)
+            {
+                int v = nodeQueue.Dequeue();
+                for (int c = 0; c < GetAdjacencyMatrix().GetLength(0); c++)
+                {
+                    if (!visited[c] && GetAdjacencyMatrix()[v, c] > 0)    //c jest nieodwiedzony i c jest sąsiadem v
+                    {
+                        nodeQueue.Enqueue(c);
+                        if (GetAdjacencyMatrix()[v, c] + GetDistance()[v] < GetDistance()[c])
+                        {
+                            SetDistance(c, GetAdjacencyMatrix()[v, c] + GetDistance()[v]);
+                            if (v != cityNum)
+                            {
+                                waypoints[c].Clear();
+                                waypoints[c].InsertRange(0, waypoints[v]);  //punkty pośrednie od cityNum do v
+                                waypoints[c].Add(v);
+                            }
+                        }
+                    }
+                }
+                visited[v] = true;
+            }
+            return waypoints;
+        }
+    }
+}
---
+++ AlgorytmDijkstry/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmDijkstry
+{
+    class Program
+    {
+        public static string[] cityNames = {"Kraków", "Rzeszów", "Gdańsk", "Wrocław", "Warszawa", "Łódź", "Bydgoszcz",
+            "Szczecin", "Lublin", "Opole"};
+        public static int ChooseCity(string[] cityNames)
+        {
+            Console.WriteLine("Wybierz miasto przesuwając kursor klawiszami w/s i zatwierdź wybór klawiszem " +
+                "enter:");
+            for (int k = 0; k < cityNames.GetLength(0); k++)
+            {
+                Console.WriteLine("\t{0}", cityNames[k]);
+            }
+            Console.SetCursorPosition(0, Console.CursorTop - cityNames.GetLength(0));
+            Console.Write(">");
+            bool choice = false;
+            int position = 0;
+            while (!choice)
+            {
+                char cursor = Console.ReadKey(true).KeyChar;
+                switch (cursor)
+                {
+                    case 'w':
+                        if (position > 0)
+                        {
+                            Console.Write("\b \b");
+                            Console.SetCursorPosition(0, Console.CursorTop - 1);
+                            Console.Write(">");
+                            position--;
+                        }
+                        break;
+                    case 's':
+                        if (position < cityNames.GetLength(0) - 1)
+                        {
+                            Console.Write("\b \b");
+                            Console.SetCursorPosition(0, Console.CursorTop + 1);
+                            Console.Write(">");
+                            position++;
+                        }
+                        break;
+                    case (char)13:
+                        Console.SetCursorPosition(0, Console.CursorTop + cityNames.GetLength(0) - position);
+                        choice = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return position;
+        }
+        public static void PrintRoutes(string[] cities, int cityNum, double[] distance, List<int>[] waypoints)
+        {
+            for (int l = 0; l < waypoints.GetLength(0); l++)
+            {
+                if (l == cityNum)
+                    Console.WriteLine("{0} jest miastem startowym. Odległość wynosi {1} km.", cities[l], distance[l]);
+                else if (waypoints[l].Count == 0)
+                    Console.WriteLine("Najkrótsza droga do miasta {1} prowadzi bezpośrednio z " +
+                        "miasta {0} i wynosi {2} km.", cities[cityNum], cities[l], distance[l]);
+                else
+                {
+                    Console.Write("Najkrótsza droga do miasta {0} prowadzi przez następujące miasta: ", cities[l]);
+                    foreach (int waypoint in waypoints[l])
+                        Console.Write("{0}, ", cities[waypoint]);
+                    Console.WriteLine("\b\b. Trasa ma długość {0} km.", distance[l]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dijkstra di = new Dijkstra(10);
+            di.CreateEdges();
+            int cityNumber = ChooseCity(cityNames);
+            PrintRoutes(cityNames, cityNumber, di.GetDistance(), di.CalculateDistance(cityNumber));
+
+            Console.Write("\nNaciśnij 1, aby wyświetlić macierz sąsiedztwa lub naciśnij dowolny klawisz," +
+                " aby zakończyć program.");
+            bool display = false;
+            while (true)
+            {
+                char matrix = Console.ReadKey().KeyChar;
+                switch (matrix)
+                {
+                    case '1':
+                        if (!display)
+                        {
+                            Console.WriteLine();
+                            di.DisplayAdjacencyMatrix();
+                            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć program.");
+                            display = true;
+                        }
+                        else
+                            Environment.Exit(0);
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
